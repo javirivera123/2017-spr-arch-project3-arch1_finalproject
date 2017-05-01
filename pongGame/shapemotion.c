acee8dff7618d4bf544c4d79ff975e0ac4c30a56
Javier Rivera <jrivera17@miners.utep.edu>
2017-04-30 20:28:54 -0600

    add score to screen

--- pongGame/shapemotion.c
+++ pongGame/shapemotion.c
@@ -5,9 +5,9 @@
  *  While the CPU is running the green LED is on, and
  *  when the screen does not need to be redrawn the CPU
  *  is turned off along with the green LED.
- */  
-#include <msp430.h>
+ */
 #include <stdio.h>
+#include <msp430.h>
 #include <libTimer.h>
 #include <lcdutils.h>
 #include <lcddraw.h>
