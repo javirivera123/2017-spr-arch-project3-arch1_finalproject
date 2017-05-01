41def0647da0aa26185878defcbfdcb2dd99083a
Javier Rivera <jrivera17@miners.utep.edu>
2017-05-01 10:58:34 -0600

    \adjusted score to add

--- pongGame/shapemotion.c
+++ pongGame/shapemotion.c
@@ -273,7 +273,7 @@ void wdt_c_handler()
   P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
   count ++;
   if (count == 15) {
-    mlAdvance(&ml0, &fieldFence);
+    detectCollisions(&ml0, &fieldFence);
     if (p2sw_read())
       redrawScreen = 1;
     count = 0;
