4e183bba1d219cb74d06df89d9c83d9dc0bd6f66
Javier Rivera <jrivera17@miners.utep.edu>
2017-05-02 18:57:32 -0600

    placing thing in wdt

--- pongGame/shapemotion.c
+++ pongGame/shapemotion.c
@@ -1,5 +1,4 @@
-/** \file shapemotion.c
- *  \brief This is a simple shape motion demo.
+ /*  \brief This is a simple shape motion demo.
  *  This demo creates two layers containing shapes.
  *  One layer contains a rectangle and the other a circle.
  *  While the CPU is running the green LED is on, and
@@ -38,35 +37,33 @@ AbRect rectanglePanel = {
 
 
 AbRectOutline fieldOutline = {	/* playing field */
-  abRectOutlineGetBounds, abRectOutlineCheck,   
+  abRectOutlineGetBounds, abRectOutlineCheck,
   {screenWidth/2-5, screenHeight/2-5}
 };
+ 
+ Layer fieldLayerL3 = {		/* playing field as a layer */
+         (AbShape *) &fieldOutline,
+         {screenWidth/2, screenHeight/2},/**< center */
+         {0,0}, {0,0},				    /* last & next pos */
+         COLOR_BLACK,
+         &BallLayerL2,
+ };
 
-
-
-Layer BallLayerL3 = {		/** Layer with a violet Ball */
+Layer BallLayerL2 = {		/** Layer with a violet Ball */
   (AbShape *) &circle8,
   {(screenWidth/2)+10, (screenHeight/2)+5}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
   COLOR_VIOLET,
   0,
 };
-
-
-Layer fieldLayerL2 = {		/* playing field as a layer */
-  (AbShape *) &fieldOutline,
-  {screenWidth/2, screenHeight/2},/**< center */
-  {0,0}, {0,0},				    /* last & next pos */
-  COLOR_BLACK,
-  &BallLayerL3,
-};
+ 
 
 Layer leftPadL1 = {		/**< Layer with left pad */
   (AbShape *)&rectanglePanel,
   {(screenWidth/2)-49, (screenHeight/2)+8}, /**< left */
   {0,0}, {0,0},				    /* last & next pos */
   COLOR_BLACK,
-  &fieldLayerL2,
+  &fieldLayerL3,
 };
 
 Layer rightPadL0 = {		/**< Layer with right pad */
@@ -87,7 +84,7 @@ typedef struct MovLayer_s {
   struct MovLayer_s *next;
 } MovLayer;
 
-MovLayer ml3 = { &BallLayerL3, {-1,2}, 0 };//layer for ball
+MovLayer ml3 = { &BallLayerL2, {-1,2}, 0 };//layer for ball
 MovLayer ml1 = { &leftPadL1, {0,1}, &ml3 };//left paddle
 MovLayer ml0 = { &rightPadL0, {0,1}, &ml1 };//right paddle
 
@@ -110,21 +107,21 @@ void movLayerDraw(MovLayer *movLayers, Layer *layers)
   for (movLayer = movLayers; movLayer; movLayer = movLayer->next) { /* for each moving layer */
     Region bounds;
     layerGetBounds(movLayer->layer, &bounds);
-    lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1], 
+    lcd_setArea(bounds.topLeft.axes[0], bounds.topLeft.axes[1],
 		bounds.botRight.axes[0], bounds.botRight.axes[1]);
     for (row = bounds.topLeft.axes[1]; row <= bounds.botRight.axes[1]; row++) {
       for (col = bounds.topLeft.axes[0]; col <= bounds.botRight.axes[0]; col++) {
 	Vec2 pixelPos = {col, row};
 	u_int color = bgColor;
 	Layer *probeLayer;
-	for (probeLayer = layers; probeLayer; 
+	for (probeLayer = layers; probeLayer;
 	     probeLayer = probeLayer->next) { /* probe all layers, in order */
 	  if (abShapeCheck(probeLayer->abShape, &probeLayer->pos, &pixelPos)) {
 	    color = probeLayer->color;
-	    break; 
+	    break;
 	  } /* if probe check */
 	} // for checking all layes at col, row
-	lcd_writeColor(color); 
+	lcd_writeColor(color);
       } // for col
     } // for row
   } // for moving layer being updated
@@ -153,28 +150,28 @@ void mlAdvance(MovLayer *ml, Region *fence)
 //Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Create a fence region */
 
 /** Advances a moving shape within a fence
- *  
+ *
  *  \param ml The moving shape to be advanced
  *  \param fence The region which will serve as a boundary for ml
  */
-void detectCollisions( Layer *rightPadL0, Layer *leftPadL1, Layer *BallLayerL3, MovLayer *ml, Region *fence )
+void detectCollisions( Layer *rightPadL0, Layer *leftPadL1, Layer *BallLayerL2, MovLayer *ml, Region *fence )
 {
   Vec2 newPos;
   u_char axis;
   Layer *Pad1 = rightPadL0;
   Layer *Pad2 = leftPadL1;
-  Layer *Ball = BallLayerL3;
+  Layer *Ball = BallLayerL2;
 
   Region shapeBoundary;
     vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
     abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
 
-    if (((shapeBoundary.topLeft.axes[0] <= fence->botRight.axes[0]) &&  //ball bouncing
-         (shapeBoundary.topLeft.axes[1] > fence->topLeft.axes[1]) &&
-         (shapeBoundary.topLeft.axes[1] < fence->botRight.axes[1]))||
-        ((shapeBoundary.botRight.axes[0] >= fence->topLeft.axes[0]) &&
-         (shapeBoundary.botRight.axes[1] > fence->topLeft.axes[1]) &&
-         (shapeBoundary.botRight.axes[1] < fence->botRight.axes[1]))) {
+    if (((shapeBoundary.topLeft.axes[0] <= fenceP1->botRight.axes[0]) &&  //ball bouncing
+         (shapeBoundary.topLeft.axes[1] > fenceP1->topLeft.axes[1]) &&
+         (shapeBoundary.topLeft.axes[1] < fenceP1->botRight.axes[1]))||
+        ((shapeBoundary.botRight.axes[0] >= fenceP2->topLeft.axes[0]) &&
+         (shapeBoundary.botRight.axes[1] > fenceP2->topLeft.axes[1]) &&
+         (shapeBoundary.botRight.axes[1] < fenceP2->botRight.axes[1]))) {
         int velocity = ml->velocity.axes[0] = -ml->velocity.axes[0];
         newPos.axes[0] += (2*velocity);
        }	/**< if outside of fence */
@@ -256,7 +253,7 @@ int redrawScreen = 1;           /**< Boolean for whether screen needs to be redr
 Region fieldFence;		/**< fence around playing field  */
 
 
-/** Initializes everything, enables interrupts and green LED, 
+/** Initializes everything, enables interrupts and green LED,
  *  and handles the rendering for the screen
  */
 void main() {
@@ -275,7 +272,7 @@ void main() {
   layerDraw(&rightPadL0);
 
 
-  layerGetBounds(&fieldLayerL2, &fieldFence);
+  layerGetBounds(&fieldLayerL3, &fieldFence);
 
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);                  /**< GIE (enable interrupts) */
@@ -292,13 +289,7 @@ void main() {
   score2[3] = 0;
 
   for (;;) {
-
-      layerGetBounds(&rightPadL0, &fieldLayerL2);
-      layerGetBounds(&leftPadL1, &fieldLayerL2);
-
-      //movLayerDraw(&ml3, &BallLayerL3);
-
-    //while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
+    while (!redrawScreen) { /**< Pause CPU if screen doesn't need updating */
      // P1OUT &= ~GREEN_LED;    /**< Green led off witHo CPU */
      // or_sr(0x10);          /**< CPU OFF */
    // }
@@ -363,7 +354,7 @@ void wdt_c_handler()
   P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
   count ++;
   //if (count == 15) {
-    detectCollisions(&leftPadL1, &rightPadL0, &BallLayerL3, &ml0, &fieldFence);
+    detectCollisions(&leftPadL1, &rightPadL0, &BallLayerL2, &ml0, &fieldFence);
     u_int sw = p2sw_read();
     buttonSense(sw, &ml1, &ml0);
 
