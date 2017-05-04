4f84e106156168255fe600177c044ee1072722de
Javier Rivera <jrivera17@miners.utep.edu>
2017-05-04 14:16:34 -0600

    just one paddle working

--- pongGame/shapemotion.c
+++ pongGame/shapemotion.c
@@ -120,7 +120,7 @@ void movLayerDraw(MovLayer *movLayers, Layer *layers)
   } // for moving layer being updated
 }
 
-Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Create a fence region */
+//Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Create a fence region */
 
 /** Advances a moving shape within a fence
  *
@@ -128,24 +128,23 @@ Region fence = {{10,30}, {SHORT_EDGE_PIXELS-10, LONG_EDGE_PIXELS-10}}; /**< Crea
  *  \param fence The region which will serve as a boundary for ml
  */
 void mlAdvance(MovLayer *ml, Region *fence)
- {
-     Vec2 newPos;
-     u_char axis;
-     Region shapeBoundary;
-     for (; ml; ml = ml->next) {
-         vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
-         abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
-         for (axis = 0; axis < 2; axis ++) {
-             if ((shapeBoundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) ||
-                 (shapeBoundary.botRight.axes[axis] > fence->botRight.axes[axis]) ) {
-                 int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
-                 newPos.axes[axis] += (2*velocity);
-             }	/**< if outside of fence */
-         } /**< for axis */
-         ml->layer->posNext = newPos;
-     } /**< for ml */
- }
-
+{
+    Vec2 newPos;
+    u_char axis;
+    Region shapeBoundary;
+    for (; ml; ml = ml->next) {
+        vec2Add(&newPos, &ml->layer->posNext, &ml->velocity);
+        abShapeGetBounds(ml->layer->abShape, &newPos, &shapeBoundary);
+        for (axis = 0; axis < 2; axis ++) {
+            if ((shapeBoundary.topLeft.axes[axis] < fence->topLeft.axes[axis]) ||
+                (shapeBoundary.botRight.axes[axis] > fence->botRight.axes[axis]) ) {
+                int velocity = ml->velocity.axes[axis] = -ml->velocity.axes[axis];
+                newPos.axes[axis] += (2*velocity);
+            }	/**< if outside of fence */
+        } /**< for axis */
+        ml->layer->posNext = newPos;
+    } /**< for ml */
+}
 
 
 
@@ -192,8 +191,9 @@ void switchHandler(u_int switches){
             }
         }
 
- Region fieldFence;		/**< fence around playing field  */
+
  int redrawScreen = 1;           /**< Boolean for whether screen needs to be redrawn */
+Region fieldFence;		/**< fence around playing field  */
 /** Initializes everything, enables interrupts and green LED,
  *  and handles the rendering for the screen
  */
